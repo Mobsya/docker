f22616d4bc5c6fe0771c636f9b1c19eb0029293b
philippe.docourt <philippe.docourt@digital-solutions.io>
2021-02-03 19:31:22 +0100

    Added utility scripts for setting up Android environment and building Thymio Suite.

---
+++ android/build-thymio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Setup env and load variable in current shell.
+./setup-thymio-dev-env.sh && . .env
+
+# Build all targets
+build_types="Debug Release"
+for build_type in $build_types; do
+    architectures="armeabi-v7a arm64-v8a x86 x86_64"
+    for arch in $architectures; do
+        mkdir -p ${BUILD_DIR}/${build_type}/${arch}/android-${API_LEVEL}
+        docker run --rm \
+        	-v ${SRC_DIR}:/src:rw \
+        	-v ${BUILD_DIR}/${build_type}/${arch}/android-${API_LEVEL}:/build:rw \
+        	mobsya/thymio-dev-env:${DEV_ENVIRONMENT_TAG} \
+        	"cmake -DANDROID_PLATFORM=android-${API_LEVEL} -DANDROID_ABI=${arch} -DCMAKE_TOOLCHAIN_FILE=\$ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake -DCMAKE_FIND_ROOT_PATH=/qt -DCMAKE_BUILD_TYPE=$build_type -DBUILD_SHARED_LIBS=OFF -GNinja -S /src -B /build && cd /build && ninja -j $(nproc)"
+    done
+done
---
+++ android/setup-thymio-dev-env.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+API_LEVEL=21
+DEV_ENVIRONMENT_TAG=android${API_LEVEL}-ndk21-qt5.15.2-cmake3.19.3
+
+if [ ! -f "$SRC_DIR/CMakeLists.txt" ]; then
+  echo --- "$SRC_DIR/CMakeLists.txt" not found. We need to define the source dir.
+  SRC_DIR=
+fi
+if [ -z "$SRC_DIR" ]; then
+  read -p "Enter path to the source root folder: " SRC_DIR
+fi
+if [ ! -f "$SRC_DIR/CMakeLists.txt" ]; then
+  echo --- "$SRC_DIR/CMakeLists.txt" not found. Aborting.
+  exit 1
+fi
+if [ -z "$BUILD_DIR" ]; then
+  read -p "Enter path to the build root folder: " BUILD_DIR
+fi
+if [ -z "$BUILD_DIR" ]; then
+  echo --- The build folder is undefined. Aborting.
+  exit 1
+fi
+
+if [ ! -d "$SRC_DIR/vpl3-thymio-suite" ]; then
+  echo ---Download assets into ${SRC_DIR}
+  vpl3_url="https://github.com/Mobsya/ci-data/releases/download/data/vpl3-thymio-suite.tar.gz"
+  wget ${vpl3_url} && tar xzf vpl3-thymio-suite.tar.gz -C ${SRC_DIR}
+  rm vpl3-thymio-suite.tar.gz
+fi
+if [ ! -d "$SRC_DIR/scratch" ]; then
+  echo ---Download assets into ${SRC_DIR}
+  scratch_version="v20201116.1"
+  scratch_url="https://github.com/Mobsya/scratch-gui/releases/download/${scratch_version}/scratch-gui.tar.gz"
+  wget ${scratch_url} && tar xzf scratch-gui.tar.gz -C ${SRC_DIR}
+  rm scratch-gui.tar.gz
+fi
+
+echo API_LEVEL=$API_LEVEL > .env
+echo DEV_ENVIRONMENT_TAG=$DEV_ENVIRONMENT_TAG  >> .env
+echo SRC_DIR=$SRC_DIR >> .env
+echo BUILD_DIR=$BUILD_DIR >> .env
+
+echo --- Type . .env to source the environment variables.
